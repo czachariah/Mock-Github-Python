2df10297a908899360e4def76f0b6ec3ad4e0996
czachariah <zachariahchris@yahoo.com>
2020-05-15 15:15:10 -0400

    Some changes to the structure

---
+++ Client/client.py
@@ -0,0 +1,10 @@
+# This is the client program.
+# Author: Chris Zachariah - chriszachariah3@gmail.com
+
+
+def client():
+	print("[Client]: Client Program Started!")
+	print("[Client]: What do you want to do?")
+
+if __name__ == "__main__":
+	client()
---
+++ Server/server.py
@@ -0,0 +1,10 @@
+# This is the server program.
+# Author: Chris Zachariah - chriszachariah3@gmail.com
+
+
+def server():
+        print("[Server]: Client Program Started!")
+        print("[Server]: What do you want to do?")
+
+if __name__ == "__main__":
+        server()
