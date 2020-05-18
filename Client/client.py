ffa5ecbd9ffcc3e21a55dbe0cd64461cce001afd
czachariah <zachariahchris@yahoo.com>
2020-05-18 14:19:35 -0400

    set up some simple commands and structure for C and S

--- Client/client.py
+++ Client/client.py
@@ -3,6 +3,7 @@
 
 import socket
 import sys
+import os
 
 
 def client():
@@ -10,7 +11,7 @@ def client():
         print("[C]: ERROR: Not enough arguments given. Please try again.")
         exit()
     if sys.argv[1] == "configure":
-        print("[C]: configure")
+        configure()
     elif sys.argv[1] == "checkout":
         print("[C]: checkout")
     elif sys.argv[1] == "update":
@@ -22,7 +23,7 @@ def client():
     elif sys.argv[1] == "push":
         print("[C]: push")
     elif sys.argv[1] == "create":
-        print("[C]: create")
+        create()
     elif sys.argv[1] == "destroy":
         print("[C]: destroy")
     elif sys.argv[1] == "add":
@@ -38,9 +39,50 @@ def client():
     else:
         print("[C]: ERROR: Invalid command used. Please try again.")
         exit()
-
     print("[C]: Closing client program.")
     exit()
+
+
+def configure():
+    if len(sys.argv) != 4:
+        print("[C]: ERROR: Please make sure to include the HostName/IPAddress and PortNumber of the server.")
+        return
+
+    serverAdd = socket.gethostbyname(sys.argv[2])
+    serverPort = int(sys.argv[3])
+    if serverPort <= 1023:
+        print("[C]: ERROR: Need to make sure that the port number is strictly greater thn 1023. Please try again.\n")
+        return
+
+    # try to find a RESOLVED.txt, delete it if it exists and then make a new one to write and append to
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    configFile = os.path.join(dir_name, "config" + "." + "txt")
+    if os.path.exists(configFile):
+        os.remove(configFile)
+    cfd = open("config.txt", "a+")
+
+    cfd.write(serverAdd + "\n")
+    cfd.write(str(serverPort) + "\n")
+
+    print("[C]: Server address and port number have been added to the config.txt file.")
+
+
+def create():
+    if len(sys.argv) != 3:
+        print("[C]: ERROR: Please make sure to include the project name for the argument.")
+        return
+
+    addressAndPort = list()
+    try:
+        configFD = open("config.txt", "r")
+        for line in configFD:
+            line = line.replace("\r", "").replace("\n", "")
+            addressAndPort.append(line)
+    except IOError:
+        print("[C]: ERROR: Must first run the configure command before trying to connect with the server.")
+        return
+
+    print(addressAndPort)
 
 
 if __name__ == '__main__':
--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,53 @@
 # This is the server program.
 # Author: Chris Zachariah - chriszachariah3@gmail.com
 
+import socket
+import sys
+from _thread import *
+import threading
+
+lock = threading.Lock()
 
 def server():
-        print("[Server]: Client Program Started!")
-        print("[Server]: What do you want to do?")
+        if len(sys.argv) != 2:
+                print("[S]: Please make sure to the port number in the arguments.\n")
+                exit()
+
+        portNum = int(sys.argv[1])
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_binding = ('', portNum)
+        s.bind(server_binding)
+        print("[S]: Socket created and bound to port: " + sys.argv[1])
+
+        # put the socket into listening mode
+        s.listen(8)
+        print("[S]: Socket is listening ... \n")
+
+        # a forever loop until client wants to exit
+        while True:
+                # establish connection with client
+                c, addr = s.accept()
+
+                # lock acquired by client
+                lock.acquire()
+                print('[S]: Connected to: ', addr[0], ' : ', addr[1], '\n')
+
+                # Start a new thread and return its identifier
+                start_new_thread(serverAction, c)
+        s.close()
+
+
+def serverAction(c):
+        data = c.recv(1024)
+        print("[S]: From client: " + data)
+
+        c.send(data + "received")
+
+        lock.release()
+
+        c.close()
+        print("[S]: Client connection dealt with and terminated.\n")
+
 
 if __name__ == "__main__":
         server()
